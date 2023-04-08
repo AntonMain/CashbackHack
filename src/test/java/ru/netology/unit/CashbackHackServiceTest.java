9e76e1b6a1527444000a07980cf3cf7bba56d628
Anton Meshkov <vigrantario@gmail.com>
2023-04-08 12:42:05 +0300

    Testng1

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,17 +1,2 @@
-package ru.netology.unit;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class CashbackHackServiceTest {
-
-    @Test
-    void remain() {
-        int amount = 2000;
-        int expected = 1000;
-        CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(amount);
-        assertEquals(expected, actual);
-    }
+package ru.netology.unit;public class CashbackHackServiceTest {
 }
