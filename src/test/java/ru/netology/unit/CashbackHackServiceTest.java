8b851ef903acb412847fc8ea42ee2af1fb23ca19
Anton Meshkov <vigrantario@gmail.com>
2023-04-08 12:43:00 +0300

    Testng1

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,2 +1,26 @@
-package ru.netology.unit;public class CashbackHackServiceTest {
+package ru.netology.unit;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void IfDefiniteAmountCashbackTest() {
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(service.remain(3500), 500);
+    }
+
+    @Test
+    public void IfZeroAmountForCashbackTest() {
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void IfMinAmountForCashbackTest() {
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(service.remain(1000), 0);
+    }
 }
